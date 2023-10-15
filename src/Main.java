67ff7b573b1d934fdedafed750b9b83664e51e6b
MIHNEA-CPC\mihne <mihneabostina5@gmail.com>
2023-10-16 00:52:16 +0300

    CHESS_BOARD PROBLEM

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("HELLO ");
         //CHESSBOARD PROBLEM
         ChessBoardProblem cbp = new ChessBoardProblem();
         System.out.println("Numarul total de boabe de pe tabla de sah este : " + cbp.getTotalBoabe());
