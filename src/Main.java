a48c12d6c2f799010f8707237569afba46d5bb45
MIHNEA-CPC\mihne <mihneabostina5@gmail.com>
2023-10-15 23:08:02 +0300

    more_stuff

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        String name = "Mihai";
+        System.out.println(name.toUpperCase());
+        float salut=136_23.01f;
+        salut++;
+        System.out.println("salut = " + salut);
+        Date currentDate = new Date();
+        System.out.println("data curenta = " + currentDate);
+        String newString = "Hiii";
+        System.out.println(newString + " cacat");
+        String newString2 = newString.replace('i','b');
+        System.out.println(newString2 + ' ' + newString);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
